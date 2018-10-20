d4292e4ff09db7d1d59cb7e31c836b85ac6a444c
ASIF ANSARI <ashifa454@gmail.com>
2018-10-21 00:19:13 +0530

    Changes

--- src/components/index.js
+++ src/components/index.js
@@ -4,30 +4,23 @@ import { instruments } from "../utils/layoutConfig";
 
 import Grid from "./Grid";
 class App extends Component {
-
   getInstrumentsName = () => {
-    return instruments.map(instrument => <li key={instrument.name}>{instrument.name.replace(/_/g, ' ')}</li>);
+    return instruments.map(instrument => (
+      <li key={instrument.name}>{instrument.name.replace(/_/g, " ")}</li>
+    ));
   };
   render() {
     return (
       <div className="App">
         <h1 className="heading">Midi Mixer</h1>
         <div className="music-container">
-          <ul className="instruments-list">
-            {this.getInstrumentsName()}
-          </ul>
+          {/* <ul className="instruments-list">{this.getInstrumentsName()}</ul> */}
           <Grid />
         </div>
         <div className="button-group">
-            <button>
-                Play
-            </button>
-            <button>
-                Stop
-            </button>
-            <button>
-                Pause
-            </button>
+          <button>Play</button>
+          <button>Stop</button>
+          <button>Pause</button>
         </div>
       </div>
     );
